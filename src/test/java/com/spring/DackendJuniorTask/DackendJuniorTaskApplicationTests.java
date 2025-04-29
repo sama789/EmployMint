684f10f1daac3985ec6a928c4e1a065aaeda4963
Samah <94363434+sama789@users.noreply.github.com>
2025-04-29 12:08:37 +0200

    Update EmployMintApplicationTests.java

--- src/test/java/com/spring/DackendJuniorTask/DackendJuniorTaskApplicationTests.java
+++ src/test/java/com/spring/DackendJuniorTask/DackendJuniorTaskApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DackendJuniorTaskApplicationTests {
+class EmployMintApplicationTests {
 
 	@Test
 	void contextLoads() {
